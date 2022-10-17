63d4a5e3e084378f8d2a73a225648bcd32784710
appnwebdeveloper7 <appnwebdeveloper7@gmial.com>
2022-10-17 17:55:57 +0530

    event

--- controllers/Event/getLatestEventController.js
+++ controllers/Event/getLatestEventController.js
@@ -3,7 +3,7 @@ const Event = require('../../models/Event')
 exports.getLatestEvent = async(req, res) => {
     try{
         const today = new Date()
-        const events = await Event.find({$and:[{startdate: {$lte: today}},{enddate: {$gte: today}}]})
+        const events = await Event.find({enddate: {$gte: today}})
         res.status(201).send({events: events})
     }catch(error){
         res.send({error: error.message})
