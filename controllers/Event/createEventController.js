3618fd47c720ef77ffab1ca3511855cce930a704
appnwebdeveloper7 <appnwebdeveloper7@gmial.com>
2022-10-18 18:14:01 +0530

    uploadevent

--- controllers/Event/createEventController.js
+++ controllers/Event/createEventController.js
@@ -6,11 +6,20 @@ const unlinkAsync = promisify(fs.unlink)
 
 exports.createEvent = async(req, res) => {
     try{
+        console.log("req.file...",req.file)
+        console.log("req.files...",req.files)
         let p_array = []
         for (let index = 0; index < req.files.length; index++) {
             const element = req.files[index].path;
-            const image = await cloudinary.uploader.upload(element,{folder: 'eventphotos'})
-            const photo = {pid: image.public_id, purl: image.secure_url, thumbnail: image.secure_url.replace("/image/upload","/image/upload/c_fill,w_300,h_300")}
+            let image;
+            let photo;
+            if(req.files[index].mimetype.split("/")[0]==="image"){
+                image = await cloudinary.uploader.upload(element,{folder: 'eventphotos'})
+                photo = {pid: image.public_id, purl: image.secure_url, thumbnail: image.secure_url.replace("/image/upload","/image/upload/c_fill,w_300,h_300")}
+            }else{
+                image = await cloudinary.uploader.upload(req.files[0].path,{resource_type: "video",folder: 'eventphotos'})
+                photo = {pid: image.public_id, purl: image.secure_url, thumbnail: image.secure_url}
+            }
             p_array.push(photo)
             await unlinkAsync(element)
         }
